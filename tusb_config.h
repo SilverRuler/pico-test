565aec9fbe30e655b4e977987d7d351a4b27809a
SilverRuler <tyeun7@gmail.com>
2025-04-29 23:57:50 +0900

    action6

--- tusb_config.h
+++ tusb_config.h
@@ -1,12 +1,24 @@
 #ifndef _TUSB_CONFIG_H_
 #define _TUSB_CONFIG_H_
 
+#ifndef CFG_TUSB_MCU
 #define CFG_TUSB_MCU             OPT_MCU_RP2040
-#define CFG_TUSB_RHPORT0_MODE    OPT_MODE_DEVICE
-#define CFG_TUSB_OS              OPT_OS_NONE
-#define CFG_TUSB_DEBUG           0
+#endif
 
+#ifndef CFG_TUSB_RHPORT0_MODE
+#define CFG_TUSB_RHPORT0_MODE    OPT_MODE_DEVICE
+#endif
+
+#ifndef CFG_TUSB_OS
+#define CFG_TUSB_OS              OPT_OS_NONE
+#endif
+
+#ifndef CFG_TUD_HID
 #define CFG_TUD_HID              1
+#endif
+
+#ifndef CFG_TUD_HID_EP_BUFSIZE
 #define CFG_TUD_HID_EP_BUFSIZE   64
+#endif
 
 #endif /* _TUSB_CONFIG_H_ */
